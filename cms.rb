75ad6203c16a4da4154fdc97ca1a57d1cd0e5371
Ainaa Azman <ainaasakinah@gmail.com>
2021-06-21 16:24:37 +0900

    add tests in isolation to production

--- cms.rb
+++ cms.rb
@@ -3,11 +3,17 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'redcarpet'
 
-root = File.expand_path("..", __FILE__)
-
 configure do
   enable :sessions
   set :sessions_secret, 'secret'
+end
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data/, __FILE__")
+  else
+    File.expand_path("../data", __FILE__)
+  end
 end
 
 def render_markdown(text)
@@ -17,28 +23,32 @@ end
 
 def load_file_content(path)
   content = File.read(path)
-  if File.extname(path) == ".md"
+  case File.extname(path)
+  when ".md"
     render_markdown(content)
-  else
+  when ".txt"
     headers["Content-Type"] = "text/plain"
     content
   end
 end
 
 get '/' do
-  @files = Dir.glob(root + '/data/*').map { |path| File.basename(path) }
-
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do|path|
+    File.basename(path)
+  end
   erb :index
 end
 
 # Open a document as text file
 
 get '/:filename' do
-  file_path = root + '/data/' + params[:filename]
+  file_path = File.join(data_path, params[:filename])
+
   if File.exist? file_path
     load_file_content(file_path)
   else
-    session[:error] = "#{params[:filename]} does not exist."
+    session[:message] = "#{params[:filename]} does not exist."
     redirect '/'
   end
 end
@@ -46,13 +56,14 @@ end
 # Edit an existing document
 
 get '/:filename/edit' do
-  file_path = root + '/data/' + params[:filename]
+  file_path = File.join(data_path, params[:filename])
+  @filename = params[:filename]
 
   if File.exist? file_path
     @content = File.read(file_path)
     erb :edit
   else
-    session[:error] = "#{params[:file_name]} does not exist."
+    session[:message] = "#{@filename} does not exist."
     redirect '/'
   end
 end
@@ -60,46 +71,22 @@ end
 # Updates an existing document
 
 post '/:filename' do
-  file_path = root + '/data/' + params[:filename]
-  File.open(file_path, "w") { |f| f.write(params[:file_content]) }
+  file_path = File.join(data_path, params[:filename])
+
+  File.open(file_path, "w") { |file| file.write(params[:file_content]) }
 
   session[:message] = "#{params[:filename]} has been updated."
   redirect "/"
 end
 
 =begin
-Problem: Editing Dcoument Content
-- Allow users to modify the content stored in the CMS
+How to test in isolation
+- Currently, overwriting the files and test would file
+To solve this, we can:
+- using two different directories, one for testing and the other for prod
+- if database, use two different database
+- we can use two different directories
 
-Questions:
-- How does that impact testing if the content is changeable??
-
-Notes:
-- Create an edit link next to each document name on the index page
-  - When edit link is clicked, they are taken to a new edit page
-    - Implement a button or hyperlink to edit page?
-- When the user views the edit page, the content should appear with a text area
-  - Render the content in the edit box?
-     - What HTML do I need for editing? Is it a form? - POST - changes data
-     - pass the content to the edit page
-     - have a button to save the chnges - POST
-        - use the content
-
-Steps to implement:
-- [x] Create an edit link next to each document - use a tags
-- [x] Create a route that takes user to edit page
-- [x] Create a view template that shows the editing form
-  - [x] Create a button to save changes
-- [x] Render the content in the edit text box
-- [x] Create a route that submits a post request with the edited content
-- [x] Create a method to save the overwrite the file with the edited content
-  - What methods can I use here? Check ruby File documents.
-
-- Create a test case for th editing page?
-  - How to test?
-  - Make two requests for before edit and after edit?
-  - Assert include should represent the before and after edit
-
-- Refactor the code to use helper methods?
+- Don't forget the SEAT approcach
 
 =end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,8 +1,11 @@
 ENV["RACK_ENV"] = "test"
 
 require 'bundler/setup'
+require "fileutils"
+
 require "minitest/autorun"
 require "rack/test"
+
 
 require_relative "../cms"
 
@@ -13,17 +16,35 @@ class CmsTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
     get '/'
 
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
   end
 
   def test_get_file
+    create_document "history.txt", "1993 - Yukihiro Matsumoto dreams up Ruby."
+
     get '/history.txt'
 
     assert_equal 200, last_response.status
@@ -46,6 +67,8 @@ class CmsTest < Minitest::Test
   end
 
   def test_markdown_as_HTML
+    create_document "about.md", "# Ruby is ..."
+
     get '/about.md'
 
     assert_equal 200, last_response.status
@@ -54,6 +77,8 @@ class CmsTest < Minitest::Test
   end
 
   def test_editing_content
+    create_document "changes.txt"
+
     get '/changes.txt/edit'
 
     assert_equal 200, last_response.status
@@ -63,6 +88,7 @@ class CmsTest < Minitest::Test
 
   def test_updating_content
     post '/changes.txt', file_content: "new content"
+
     assert_equal 302, last_response.status
 
     get last_response["Location"]
